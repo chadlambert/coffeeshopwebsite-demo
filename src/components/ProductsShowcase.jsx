f47d73ba4cb5f8b55cc8830433d22188a697ebd0
Chad Lambert <Chadchoo20@gmail.com>
2026-06-03 18:55:42 -0500

    updated components

---
+++ src/components/ProductsShowcase.jsx
@@ -0,0 +1,7 @@
+import { motion } from "framer-motion";
+import ScrollReveal, { StaggerContainer, StaggerItems } from "./ui/ScrollReveal";
+import Badge from "./ui/Badge";
+import Button from "./ui/Button";
+import Separator from "./ui/Separator";
+
+import imgEthiopianHarrar from "../assets/Ethiopian-Harrar-Bag.png";
